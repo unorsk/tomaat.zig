3cf800264127568dbb596c58edebac8cbaa7657f
andrii <25188+unorsk@users.noreply.github.com>
2023-12-21 14:31:54 +0100

    blinking

--- src/main.zig
+++ src/main.zig
@@ -11,6 +11,7 @@ var seven = [_][]const u8{ "██████", "    ██", "    ██", "  
 var eight = [_][]const u8{ "██████", "██  ██", "██████", "██  ██", "██████" };
 var nine = [_][]const u8{ "██████", "██  ██", "██████", "    ██", "██████" };
 var colon = [_][]const u8{ "  ", "██", "  ", "██", "  " };
+var colon0 = [_][]const u8{ "  ", "░░", "  ", "░░", "  " };
 
 var digits = [_][]const []const u8{ &zero, &one, &two, &three, &four, &five, &six, &seven, &eight, &nine };
 
@@ -37,21 +38,21 @@ pub fn printNumbers(m0: usize, m1: usize, s0: usize, s1: usize) !void {
 
     try stdout.print("{s}", .{"\x1B[2J"}); // clear screen
     for (range(5), 0..) |_, i| {
-        try stdout.print("{s} ", .{digits[m0][i]});
-        try stdout.print("{s} ", .{digits[m1][i]});
-        try stdout.print("{s} ", .{colon[i]});
-        try stdout.print("{s} ", .{digits[s0][i]});
-        try stdout.print("{s} \n", .{digits[s1][i]});
+        try stdout.print(" {s} ", .{digits[m0][i]});
+        try stdout.print(" {s} ", .{digits[m1][i]});
+        try stdout.print(" {s} ", .{if (@rem(s1, 2) == 1) colon[i] else colon0[i]});
+        try stdout.print(" {s} ", .{digits[s0][i]});
+        try stdout.print(" {s} \n", .{digits[s1][i]});
     }
 
     try bw.flush();
 }
 
 pub fn main() !void {
-    for (0..26) |m| {
+    for (0..25) |m| {
         for (0..60) |s| {
-            const m0 = @divTrunc(25 - m, 10);
-            const m1 = @rem(25 - m, 10);
+            const m0 = @divTrunc(24 - m, 10);
+            const m1 = @rem(24 - m, 10);
             const s0 = @divTrunc(59 - s, 10);
             const s1 = @rem(59 - s, 10);
             try printNumbers(m0, m1, s0, s1);
